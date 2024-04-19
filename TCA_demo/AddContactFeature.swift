455b0256ed2b13a41d757bd39c261b9e88e01b8c
Leon <leon.lai@bureauveritas.com>
2024-04-19 15:23:51 +0800

    first presentation

---
+++ TCA_demo/AddContactFeature.swift
@@ -0,0 +1,90 @@
+//
+//  AddContactFeature.swift
+//  TCA_demo
+//
+//  Created by lai leon on 19/4/2024.
+//
+
+import ComposableArchitecture
+import SwiftUI
+
+
+@Reducer
+struct AddContactFeature {
+    @ObservableState
+    struct State: Equatable {
+        var contact: Contact
+    }
+    enum Action {
+        case cancelButtonTapped
+        case saveButtonTapped
+        case setName(String)
+        case delegate(Delegate)
+        enum Delegate: Equatable {
+            case cancel
+            case saveContact(Contact)
+        }
+    }
+    @Dependency(\.dismiss) var dismiss
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .cancelButtonTapped:
+                return .run { _ in await self.dismiss() }
+                
+            case .saveButtonTapped:
+                return .run { [contact = state.contact] send in
+                    await send(.delegate(.saveContact(contact)))
+                    await self.dismiss()
+                }
+                
+            case let .setName(name):
+                state.contact.name = name
+                return .none
+                
+            case .delegate:
+                return .none
+            }
+        }
+    }
+}
+
+struct AddContactView: View {
+    @Bindable var store: StoreOf<AddContactFeature>
+    
+    var body: some View {
+        Form {
+            TextField("Name", text: $store.contact.name.sending(\.setName))
+        }
+        .navigationTitle("Add Contact")
+        .toolbar {
+            ToolbarItem(placement: .cancellationAction) {
+                Button("Cancel") {
+                    store.send(.cancelButtonTapped)
+                }
+            }
+            ToolbarItem(placement: .confirmationAction) {
+                Button("Save") {
+                    store.send(.saveButtonTapped)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        AddContactView(
+            store: Store(
+                initialState: AddContactFeature.State(
+                    contact: Contact(
+                        id: UUID(),
+                        name: "Blob"
+                    )
+                )
+            ) {
+                AddContactFeature()
+            }
+        )
+    }
+}
---
+++ TCA_demo/ContactsFeature.swift
@@ -0,0 +1,104 @@
+//
+//  ContactsFeature.swift
+//  TCA_demo
+//
+//  Created by lai leon on 18/4/2024.
+//
+
+import Foundation
+import ComposableArchitecture
+import SwiftUI
+
+struct Contact: Equatable, Identifiable {
+    let id: UUID
+    var name: String
+}
+
+@Reducer
+struct ContactsFeature {
+    @ObservableState
+    struct State: Equatable {
+        @Presents var addContact: AddContactFeature.State?
+        var contacts: IdentifiedArrayOf<Contact> = []
+    }
+    
+    enum Action {
+        case addButtonTapped
+        case addContact(PresentationAction<AddContactFeature.Action>)
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .addButtonTapped:
+                state.addContact = AddContactFeature.State(
+                    contact: Contact(id: UUID(), name: "")
+                )
+                return .none
+                //            case .addContact(.presented(.delegate(.cancel))):
+                //                state.addContact = nil
+                //                return .none
+            case let .addContact(.presented(.delegate(.saveContact(contact)))):
+                //                guard let contact = state.addContact?.contact
+                //                else { return .none }
+                state.contacts.append(contact)
+                //                state.addContact = nil
+                return .none
+            case .addContact:
+                return .none
+            }
+        }
+        .ifLet(\.$addContact, action: \.addContact) {
+            AddContactFeature()
+        }
+    }
+    
+}
+
+struct ContactsView: View {
+    @Bindable var store: StoreOf<ContactsFeature>
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(store.state.contacts) { contact in
+                    Text(contact.name)
+                }
+            }
+            .navigationTitle("Contacts")
+            .toolbar {
+                ToolbarItem {
+                    Button {
+                        store.send(.addButtonTapped)
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
+            .sheet(
+                item: $store.scope(state: \.addContact, action: \.addContact)
+            ) { addContactStore in
+                NavigationStack {
+                    AddContactView(store: addContactStore)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContactsView(
+        store: Store(
+            initialState: ContactsFeature.State(
+                contacts: [
+                    Contact(id: UUID(), name: "Blob"),
+                    Contact(id: UUID(), name: "Blob Jr"),
+                    Contact(id: UUID(), name: "Blob Sr"),
+                ]
+            )
+        ) {
+            ContactsFeature()
+        }
+    )
+}
+
